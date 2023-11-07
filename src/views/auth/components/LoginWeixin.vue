e3ca911ac9e77b195390b61c52b8b09b0d70d8d2
kiritoyu <yuwei@baixing.com>
2023-11-07 14:47:17 +0800

    feat: login & signup abtest

--- src/views/auth/components/LoginWeixin.vue
+++ src/views/auth/components/LoginWeixin.vue
@@ -2,6 +2,12 @@
   <div class="flex flex-col justify-center items-center w-full text-[#3D3D3D]">
     <p class="mb-[25px] text-[28px] font-medium">{{ $t('使用微信扫码登录') }}</p>
     <p class="text-sm">{{ $t('新用户扫码后，关注公众号自动创建账号') }}</p>
+    <ABTestRender :case-num="4">
+      <template #viewA> </template>
+      <template #viewB>
+        <p class="text-sm">{{ $t('（免费使用一年 赠送300电力值）') }}</p>
+      </template>
+    </ABTestRender>
     <div class="relative w-[208px] h-[208px] rounded-lg mt-10 mb-4" v-loading="loading">
       <img :src="url" :class="['w-full h-full', timeout ? 'opacity-5' : '']" />
       <el-button
@@ -20,6 +26,8 @@
 </template>
 
 <script setup lang="ts">
+import ABTestRender from '@/components/ABTestRender/index.vue'
+
 defineProps<{
   url: string
   timeout: boolean
